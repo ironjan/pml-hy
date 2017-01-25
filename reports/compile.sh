dc51e9098e527776aba253d2ade94d4ad256f807
Jan Lippert <lippertsjan@gmail.com>
2017-01-25 06:12:47 +0200

    Added compile script

---
+++ reports/compile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+pdflatex $1
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "pdflatex failed"
+  exit 1
+fi
+
+bibtex ${1::-4}
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "bibtex failed"
+  exit 1
+fi
+
+pdflatex $1
+RESULT=$?
+if [ $RESULT -nne 0 ]; then
+  echo "pdflatex 2 failed"
+  exit 1
+fi
+pdflatex $1
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+  echo "pdflatex 3 failed"
+  exit 1
+fi
+
