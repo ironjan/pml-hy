8d730d3599bef50eafaa714b7dcecf4115ddc465
Jan Lippert <lippertsjan@gmail.com>
2017-01-28 01:51:02 +0200

    Fixed evolution

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,15 +1,7 @@
 # --- !Ups
---
---CREATE TABLE User (
---    id bigint(20) NOT NULL AUTO_INCREMENT,
---    email varchar(255) NOT NULL,
---    password varchar(255) NOT NULL,
---    fullname varchar(255) NOT NULL,
---    isAdmin boolean NOT NULL,
---    PRIMARY KEY (id)
---);
+
 CREATE TABLE raw_parking_data(
-  id bigint(20) NOT NULL AUTO_INCREMENT,
+  id SERIAL,
   is_deleted boolean NOT NULL,
   name varchar(255) NOT NULL,
   used int NOT NULL,
