5dd939c5a9f800c859a6763495c2e1bf35db66bd
Jan Lippert <lippertsjan@gmail.com>
2017-02-07 22:42:20 +0200

    Implemented cleaner for single entry

--- app/services/enhancer/CleanerImpl.scala
+++ app/services/enhancer/CleanerImpl.scala
@@ -1,6 +1,9 @@
 package services.enhancer
 
+import org.joda.time.Weeks
 import services.crawler.ParkingDataSet
+
+import scala.util.Try
 
 /**
   * Implementation of Cleaner for ParkingDataSet
@@ -8,5 +11,42 @@ import services.crawler.ParkingDataSet
 class CleanerImpl extends Cleaner[ParkingDataSet] {
   override def cleanDatabase: Unit = ???
 
-  override def cleanEntry(t: ParkingDataSet): ParkingDataSet = ???
+  val recentModelVersion = Some(0)
+
+  override def cleanEntry(t: ParkingDataSet): ParkingDataSet = {
+    t.modelVersion match {
+      case None => cleanToRecent(t)
+      case `recentModelVersion` => t
+    }
+  }
+
+  private def cleanToRecent(t: ParkingDataSet) = {
+    val crawlingTime = t.crawlingTime
+    val hourOfDay = crawlingTime.getHourOfDay
+    val minuteOfHour = crawlingTime.getMinuteOfHour
+    val dayOfWeek = crawlingTime.getDayOfWeek
+    val dayOfMonth = crawlingTime.getDayOfMonth
+    val weekOfMonth = Weeks.weeksBetween(crawlingTime, crawlingTime.withDayOfMonth(1)).getWeeks() + 1
+    val weekOfYear = crawlingTime.getWeekOfWeekyear
+
+    val free = Try(t.freeRaw.toInt).toOption
+    val capacity = Try(t.capacityRaw.toInt).toOption
+
+    ParkingDataSet(crawlingTime,
+      t.name,
+      t.freeRaw,
+      t.capacityRaw,
+      t.city,
+      t.id,
+      t.isDeleted,
+      recentModelVersion,
+      Some(hourOfDay),
+      Some(minuteOfHour),
+      Some(dayOfWeek),
+      Some(dayOfMonth),
+      Some(weekOfMonth),
+      Some(weekOfYear),
+      free,
+      capacity)
+  }
 }
