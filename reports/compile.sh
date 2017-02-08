5df28c869f0d715b0d0a006f442cc02e67e65c63
Jan Lippert <lippertsjan@gmail.com>
2017-02-09 00:36:38 +0200

    Improved report compile script

--- reports/compile.sh
+++ reports/compile.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-pdflatex $1
+pdflatex $1.tex
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "pdflatex failed"
   exit 1
 fi
 
-bibtex ${1::-4}
+bibtex $1
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "bibtex failed"
   exit 1
 fi
 
-pdflatex $1
+pdflatex $1.tex
 RESULT=$?
 if [ $RESULT -nne 0 ]; then
   echo "pdflatex 2 failed"
   exit 1
 fi
-pdflatex $1
+pdflatex $1.tex
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "pdflatex 3 failed"
