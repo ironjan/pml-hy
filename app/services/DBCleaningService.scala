80355009e1d0ce2a22fc11d6de441b4aeccf33d9
Jan Lippert <lippertsjan@gmail.com>
2017-02-09 00:41:56 +0200

    Fixed cleaning rb me

---
+++ app/services/DBCleaningService.scala
@@ -0,0 +1,41 @@
+package services
+
+import java.time.Clock
+import javax.inject._
+
+import akka.actor.{Actor, _}
+import play.api.Logger
+import play.api.inject.ApplicationLifecycle
+import services.crawler.PaderbornCrawler
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+/**
+ * Implements a service that regularly cleans the DB.
+ */
+ @Singleton
+ class DBCleaningService @Inject()(crawler: PaderbornCrawler,
+                                 clock: Clock,
+                                 appLifecycle: ApplicationLifecycle) {
+  val system = ActorSystem("DBCleaningSystem")
+  val Event = "Clean"
+
+  Logger.info(s"Started application. Setting up CrawlingService.")
+
+
+  val cleaningActor = system.actorOf(Props(new Actor {
+    def receive = {
+      case Event => {
+          println(s"Triggering cleaning…")
+          crawler.crawl
+        }
+      }
+      }))
+
+  system.scheduler.schedule(10 seconds,
+    60 minutes,
+    cleaningActor,
+    Event)
+
+}
--- app/services/dbcleaner/DBCleaner.scala
+++ app/services/dbcleaner/DBCleaner.scala
@@ -3,10 +3,13 @@ package services.dbcleaner
 import com.google.inject.Inject
 
 import repository.ParkingDataRepository
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 class DBCleaner  @Inject()(repo: ParkingDataRepository){
   def removeUnneededEntries: Unit = {
     repo.getAll
-    .map(_.filter(_.isDeleteable).map(doSomething).foreach(d => Await.result(parkingDataRepository.deleteById(d.id), 5 seconds)))
+    .map(_.filter(_.isDeleteable).foreach(d => Await.result(repo.deleteById(d.id), 5 seconds)))
   }
 }
