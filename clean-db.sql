5efcdf931b48be54ae3bbfbeb12adaf4f8260ce3
Jan Lippert <lippertsjan@gmail.com>
2017-02-12 20:57:13 +0200

    Added some util sql scripts

---
+++ clean-db.sql
@@ -0,0 +1,7 @@
+﻿BEGIN;
+DELETE FROM parking_data WHERE is_deleted  = true;
+DELETE FROM parking_data WHERE model_version IS NULL OR model_version < 1;
+DELETE FROM parking_data WHERE name NOT LIKE 'P6%';
+COMMIT;
+
+SELECT COUNT(*) FROM parking_data;
---
+++ remove_duplicates.sql
@@ -0,0 +1,6 @@
+﻿DELETE FROM parking_data 
+WHERE id IN (SELECT id
+              FROM (SELECT id,
+                             ROW_NUMBER() OVER (partition BY crawling_time ORDER BY id) AS rnum
+                     FROM parking_data) t
+              WHERE t.rnum > 1)
