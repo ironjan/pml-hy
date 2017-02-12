24f2d709daa1e6cb905f1a0a1502d6fa6304dda8
Jan Lippert <lippertsjan@gmail.com>
2017-02-12 20:57:13 +0200

    Added keep-awake script to keep app awake

---
+++ keep-awake.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# times are in seconds
+SLEEP_TIME=1800
+PROD_START_TIME=020000
+PROD_END_TIME=080000
+HEROKU_APP=https://enigmatic-citadel-24582.herokuapp.com/
+
+function wake_up_app {
+  curl -I -X HEAD https://enigmatic-citadel-24582.herokuapp.com/
+}
+
+while true
+do
+  currentTime=`date +"%H%M%S"`
+  if [[ ! "$currentTime" < "$PROD_START_TIME" && ! "$currentTime" > "$PROD_END_TIME" ]]; then
+    wake_up_app
+  fi
+
+  sleep $SLEEP_TIME
+done
