e9ecb3e1ffc526370a9b2fba5026288381cebb80
Jan Lippert <lippertsjan@gmail.com>
2017-02-14 11:43:01 +0200

    Improved keep-awake
     * Fixed timezones, ie. adapted times
     * Added explanation notes
     * Formatting

--- keep-awake.sh
+++ keep-awake.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # times are in seconds
-SLEEP_TIME=1800
-PROD_START_TIME=020000
-PROD_END_TIME=080000
+SLEEP_TIME=900
+PROD_START_TIME=050000 # UTC. Opening 6am MEZ/UTC+1 MESZ/UTC+2 (switch 2017-03-27)
+PROD_END___TIME=230000 # UTC. Closing 2am MEZ/MESZ. App needs 6h sleep a day.
 HEROKU_APP=https://enigmatic-citadel-24582.herokuapp.com/
 
 function wake_up_app {
@@ -12,9 +12,9 @@ function wake_up_app {
 
 while true
 do
-  currentTime=`date +"%H%M%S"`
-  echo "Running script @$currentTime" >> ./logs/keep-awake.log
-  if [[ ! "$currentTime" < "$PROD_START_TIME" && ! "$currentTime" > "$PROD_END_TIME" ]]; then
+  currentTime=`date --utc +"%H%M%S"`
+  echo "Running script @$currentTime UTC. PID=$$" >> ./logs/keep-awake.log
+  if [[ ! "$currentTime" < "$PROD_START_TIME" && ! "$currentTime" > "$PROD_END___TIME" ]]; then
     wake_up_app
   fi
 
