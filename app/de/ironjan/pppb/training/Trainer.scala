dc9f0cafd4354e1925975efc89c8f860a23e78ea
Jan Lippert <lippertsjan@gmail.com>
2017-02-22 22:09:23 +0200

    First training with bad results

--- app/de/ironjan/pppb/training/Trainer.scala
+++ app/de/ironjan/pppb/training/Trainer.scala
@@ -5,7 +5,6 @@ import de.ironjan.pppb.core.model.ParkingDataSet
 import de.ironjan.pppb.core.repository.ParkingDataRepository
 import org.joda.time.DateTime
 import play.api.Logger
-import smile.regression.RegressionTree
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -22,22 +21,21 @@ class Trainer @Inject()(parkingDataRepository: ParkingDataRepository) {
 
     val ts = DateTime.now()
 
-    // Append checking set & predict it 
-    ds.zipWithIndex
-      .map(t => ds.slice(0, t._2 + 1))
-      .foreach(ds => trainSubset(ts, ds))
+    // Append checking set & predict it
+    val boundary =  (ds.length*3/4 + 1).toInt
+    val splitSet = (ds.slice(0, boundary), ds.slice(boundary, ds.length-1))
+    trainSubset(ts, splitSet, splitSet._1.head.capacity.get) // TODO just using get on option
 
     val totalTrainingTime = DateTime.now().getMillis - ts.getMillis
     Logger.debug(s"Total training time for all subsets: ${totalTrainingTime}ms.")
   }
 
-  private def trainSubset(ts: DateTime, ds: Seq[ParkingDataSet]) = {
-    Logger.debug(s"Training subset of length ${ds.length} (Set of $ts)  ")
+  private def trainSubset(ts: DateTime, ds: (Seq[ParkingDataSet], Seq[ParkingDataSet]), capacity: Int) = {
+    val trainingSet = ds._1
+    val testSet = ds._2
+    Logger.debug(s"Training subset of length ${trainingSet.length} and test set of length ${testSet.length} (Set of $ts)  ")
 
-    val unzipped: (Seq[Array[Double]], Seq[Double]) =
-      ds.filter(_.hasUsefulData)
-        .map(_.toMlTrainingTuple)
-        .unzip
+    val unzipped = unzipSet(trainingSet)
 
     // TODO better way for double .toArray?
     val x = unzipped._1.toArray
@@ -49,9 +47,28 @@ class Trainer @Inject()(parkingDataRepository: ParkingDataRepository) {
 
     val regressionTree = smile.regression.cart(x, y, maxNodes = 100)
 
+
     val trainingTime = System.currentTimeMillis - beforeTraining
-    Logger.debug(s"Got regression tree in ${trainingTime}ms.")
-    Logger.debug(s"Results: ${regressionTree.importance()} ${regressionTree.maxDepth()}.")
+    Logger.debug(s"Got regression tree in ${trainingTime}ms: maxDepth=${regressionTree.maxDepth()}.")
+
+    val unzipSet1 = unzipSet(testSet)
+    val aes = unzipSet1._1.map(regressionTree.predict)
+      .zip(unzipSet1._2)
+      .map(p => {
+        val yStar = p._1
+        val y = p._2
+        Logger.debug(s"Prediction: $yStar - $y")
+
+        Math.abs(yStar - y)
+      })
+    val mae = aes.sum/aes.length
+    Logger.debug(s"This tree had a mean average error of $mae.")
+  }
+
+  private def unzipSet(trainingSet: Seq[ParkingDataSet]) = {
+    trainingSet.filter(_.hasUsefulData)
+      .map(_.toMlTrainingTuple)
+      .unzip
   }
 
 }
