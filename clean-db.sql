a7bb33a6806932f19622149a32c760c1904fccd0
Jan Lippert <lippertsjan@gmail.com>
2017-02-23 17:55:13 +0200

    Updated SQL scripts

--- clean-db.sql
+++ clean-db.sql
@@ -1,7 +1,3 @@
-﻿BEGIN;
-DELETE FROM parking_data WHERE is_deleted  = true;
+﻿DELETE FROM parking_data WHERE is_deleted  = true;
 DELETE FROM parking_data WHERE model_version IS NULL OR model_version < 1;
 DELETE FROM parking_data WHERE name NOT LIKE 'P6%';
-COMMIT;
-
-SELECT COUNT(*) FROM parking_data;
