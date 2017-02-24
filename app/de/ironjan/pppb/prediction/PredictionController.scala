ac59e620f6e00d1cdfd18111cb4609c2aff279f5
Jan Lippert <lippertsjan@gmail.com>
2017-02-24 21:05:09 +0200

    Some minor work on PredictionController

--- app/de/ironjan/pppb/prediction/PredictionController.scala
+++ app/de/ironjan/pppb/prediction/PredictionController.scala
@@ -1,17 +1,31 @@
 package de.ironjan.pppb.prediction
 
+import java.time.Period
 import javax.inject.{Inject, Singleton}
 
+import de.ironjan.pppb.core.model.ParkingDataSet
+import de.ironjan.pppb.core.repository.ParkingDataRepository
 import de.ironjan.pppb.training.Trainer
+import org.joda.time.{DateTime, DurationFieldType}
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 @Singleton
-class PredictionController @Inject()(trainer: Trainer) {
+class PredictionController @Inject()(repo: ParkingDataRepository,
+                                     trainer: Trainer) {
 
   def all_crawled = Action.async { implicit request =>
+
+    repo.getAll.map {ds =>
+      val now = new DateTime();
+      val futureTime = now.withFieldAdded(DurationFieldType.minutes(), 15)
+      val futureTimeAsDoubleArray = Array()
+      val filtered = ds.filter(_.hasUsefulData)
+      val bestModel = trainer.findBestModel(filtered)
+      bestModel.predict(futureTimeAsDoubleArray)
+    }
     Future.successful(Results.NotImplemented("NIY"))
   }
 
