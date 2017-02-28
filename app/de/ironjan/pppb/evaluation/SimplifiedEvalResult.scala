5b156f7c120a8c46f377df69ce4dc4131c66a6d4
Jan Lippert <lippertsjan@gmail.com>
2017-02-28 23:32:03 +0200

    Closes #9\nUsing correct datetime format now

--- app/de/ironjan/pppb/evaluation/SimplifiedEvalResult.scala
+++ app/de/ironjan/pppb/evaluation/SimplifiedEvalResult.scala
@@ -1,15 +1,23 @@
 package de.ironjan.pppb.evaluation
 
 import de.ironjan.pppb.core.ParkingDataSetJson
+import de.ironjan.pppb.core.JsonFormats
 import org.joda.time.DateTime
 import play.api.libs.json.Json
+import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsPath, Writes}
 
 case class SimplifiedEvalResult(dateTime: DateTime, predicted: Double, actual: Double){
   def delta = Math.abs(actual - predicted)
 }
 
 object SimplifiedEvalResult{
-  implicit val simplifiedEvalResultWrites = Json.writes[SimplifiedEvalResult]
+  implicit val simplifiedEvalResultWrites: Writes[SimplifiedEvalResult] = (
+    (JsPath \ "dateTime").write[DateTime](JsonFormats.jodaDateWrites) and
+      (JsPath \ "predicted").write[Double] and
+      (JsPath \ "actual").write[Double]
+    ) (unlift(SimplifiedEvalResult.unapply))
+
   def tupled = (SimplifiedEvalResult.apply _).tupled
 
 }
