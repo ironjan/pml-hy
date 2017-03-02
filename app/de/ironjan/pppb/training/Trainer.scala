783b22e1874ac3a13381cee9164dd474eb7780d0
Jan Lippert <lippertsjan@gmail.com>
2017-03-02 20:21:36 +0200

    Doing a model massacre

--- app/de/ironjan/pppb/training/Trainer.scala
+++ app/de/ironjan/pppb/training/Trainer.scala
@@ -1,5 +1,7 @@
 package de.ironjan.pppb.training
 
+import java.lang.IllegalArgumentException
+
 import com.google.inject.Inject
 import de.ironjan.pppb.core.model.ParkingDataSet
 import de.ironjan.pppb.core.model.DateTimeHelper._
@@ -11,11 +13,15 @@ import smile.regression.Regression
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Try
 
 /**
   * Created by Jan Lippert on 19.02.2017.
   */
 class Trainer @Inject()(parkingDataRepository: ParkingDataRepository) {
+  val shrinkageStepWidth = 0.05
+
+
   def timeModelEvaluation = {
     Logger.debug(s"Started training.")
 
@@ -34,20 +40,38 @@ class Trainer @Inject()(parkingDataRepository: ParkingDataRepository) {
     val boundary = ds.length * 9 / 10
     val splitSet = (ds.slice(0, boundary), ds.slice(boundary, ds.length - 1))
 
+    var counter = 0
     // TODO just using get on option
-    evaluateModels(splitSet, splitSet._1.head.capacity.get)
+    val best = Stream.concat(
+      {
+        Logger.debug("toFullTrainingTuple")
+        evaluateModels(splitSet, splitSet._1.head.capacity.get, toFullTrainingTuple)
+      },
+      {
+        Logger.debug("toHourMinutesTuple")
+        evaluateModels(splitSet, splitSet._1.head.capacity.get, toHourMinutesTuple)
+      },
+      {
+        Logger.debug("toMinutesOfDayTuple")
+        evaluateModels(splitSet, splitSet._1.head.capacity.get, toMinutesOfDayTuple)
+      }
+    )
       .minBy(p => {
-        Logger.debug(s"Min by for $p")
-        p._1
+          Logger.debug(s"Min by for $p")
+        counter += 1
+          p._1
       })
+
+    Logger.info(s"Found that $best is the best ($counter checked in total)")
+    best
   }
 
-  private def evaluateModels(ds: (Seq[ParkingDataSet], Seq[ParkingDataSet]), capacity: Int) = {
+  private def evaluateModels(ds: (Seq[ParkingDataSet], Seq[ParkingDataSet]), capacity: Int, modelMaker: ParkingDataSet =>  (Array[Double], Double)) = {
     val trainingSet = ds._1
     val testSet = ds._2
     Logger.debug(s"Training subset of length ${trainingSet.length} and test set of length ${testSet.length}.")
 
-    val unzipped = unzipSet(trainingSet)
+    val unzipped = unzipSet(trainingSet, modelMaker)
 
     // TODO better way for double .toArray?
     val x = unzipped._1.toArray
@@ -56,31 +80,57 @@ class Trainer @Inject()(parkingDataRepository: ParkingDataRepository) {
 
     Logger.debug(s"Prepared training data.")
 
-    val shrinkageStepWidth = 0.1
     val shrinkageSteps = (1/shrinkageStepWidth - 1).toInt
     Stream.concat(
-      Stream(evaluate(smile.regression.cart(x, y, 100), testSet),
-        evaluate(smile.regression.randomForest(x, y), testSet)),
-      Stream.tabulate(shrinkageSteps) { i => (i + 1) * shrinkageStepWidth }.map(s => evaluate(smile.regression.gbm(x, y, shrinkage = s), testSet)))
+      Stream(evaluate(smile.regression.cart(x, y, 100), testSet, modelMaker),
+        try{
+          evaluate(smile.regression.randomForest(x, y), testSet, modelMaker)
+        }catch {case e: IllegalArgumentException => (Double.PositiveInfinity, null)}
+      ),
+      Stream.tabulate(shrinkageSteps) { i => (i + 1) * shrinkageStepWidth }.map(s => evaluate(smile.regression.gbm(x, y, shrinkage = s), testSet, modelMaker)))
   }
 
-  private def evaluate(regression: Regression[Array[Double]], T: Seq[ParkingDataSet]) = {
-    val xStars = unzipSet(T)._1
-    val yStars = unzipSet(T)._2
+  private def evaluate(regression: Regression[Array[Double]], T: Seq[ParkingDataSet], modelMaker: ParkingDataSet =>  (Array[Double], Double)) = {
+    try{
+      val xStars = unzipSet(T, modelMaker)._1
+      val yStars = unzipSet(T, modelMaker)._2
 
-    val aes = xStars.map(regression.predict)
-      .zip(yStars)
-      .map(p => Math.abs(p._1 - p._2))
+      val aes = xStars.map(regression.predict)
+        .zip(yStars)
+        .map(p => Math.abs(p._1 - p._2))
 
-    val mae = aes.sum / aes.length
-    Logger.debug(s"${regression} had a mean average error of $mae.")
-    (mae, regression)
+      val mae = aes.sum / aes.length
+//      Logger.debug(s"${regression} had a mean average error of $mae.")
+      (mae, regression)
+    } catch {
+      case e: IllegalArgumentException => {
+        Logger.warn(e.getMessage)
+        (Double.PositiveInfinity, null)
+      }
+    }
   }
 
-  private def unzipSet(trainingSet: Seq[ParkingDataSet]) = {
+  private def unzipSet(trainingSet: Seq[ParkingDataSet], modelMaker: ParkingDataSet =>  (Array[Double], Double)) = {
     trainingSet.filter(_.hasUsefulData)
-      .map(_.toMlTrainingTuple)
+      .map(modelMaker)
       .unzip
   }
 
+  // FIXME just using get!
+  private def toFullTrainingTuple(parkingDataSet: ParkingDataSet): (Array[Double], Double) =
+    (
+      Array(
+        parkingDataSet.hourOfDay.get.toDouble,
+        parkingDataSet.minuteOfHour.get.toDouble,
+        parkingDataSet.dayOfWeek.get.toDouble,
+        parkingDataSet.dayOfMonth.get.toDouble,
+        parkingDataSet.weekOfMonth.get.toDouble,
+        parkingDataSet.weekOfYear.get.toDouble),
+      parkingDataSet.free.get.toDouble)
+
+  private def toHourMinutesTuple(parkingDataSet: ParkingDataSet): (Array[Double], Double) =
+    (Array(parkingDataSet.hourOfDay.get, parkingDataSet.minuteOfHour.get), parkingDataSet.free.get)
+
+  private def toMinutesOfDayTuple(parkingDataSet: ParkingDataSet): (Array[Double], Double) =
+    (Array(parkingDataSet.hourOfDay.get * 60 + parkingDataSet.minuteOfHour.get), parkingDataSet.free.get)
 }
