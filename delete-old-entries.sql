50a310810ef1b6429a693ea29fdd74107d697747
Jan Lippert <lippertsjan@gmail.com>
2017-03-14 01:09:00 +0200

    Added two more util scripts

---
+++ delete-old-entries.sql
@@ -0,0 +1,5 @@
+﻿DELETE FROM predictions
+WHERE predicted_time <  NOW() - INTERVAL '2 days';
+
+DELETE FROM parking_data 
+WHERE crawling_time < NOW() - INTERVAL '7 days';
---
+++ table_counts.sql
@@ -0,0 +1,4 @@
+﻿SELECT 
+  (SELECT COUNT(*) FROM parking_data    ) AS parking_data_count,	
+  (SELECT COUNT(*) FROM predictions     ) AS predictions_count,
+  (SELECT COUNT(*) FROM play_evolutions ) AS play_evolutions_count
